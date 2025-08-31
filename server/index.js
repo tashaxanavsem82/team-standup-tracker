75d71f40553da2cc718f9e9cbbd875ddb9a967c2
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-31 03:24:25 -0500

    feat: initial commit for server/index.js

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/standuptracker', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
